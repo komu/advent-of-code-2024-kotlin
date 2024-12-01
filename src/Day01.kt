d6f1ab9336069e70b91bfadd9fe4c466a085cde0
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-01 07:16:33 +0200

    Add solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,31 @@
+import kotlin.math.abs
+
 fun main() {
+    val regex = Regex("""(\d+)\s+(\d+)""")
+
+    fun parseRow(s: String): Pair<Int, Int> =
+        regex.matchEntire(s)?.destructured?.let { (x, y) -> Pair(x.toInt(), y.toInt()) } ?: error("invalid '$s'")
+
+    fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
+        val data = input.map { parseRow(it) }
+        return Pair(data.map { it.first }, data.map { it.second })
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (xs, ys) = parse(input)
+        return xs.sorted().zip(ys.sorted()).sumOf { (x, y) -> abs(x - y) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (xs, ys) = parse(input)
+        return xs.sumOf { x -> x * ys.count { y -> x == y } }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    // Read the input from the `src/Day01.txt` file.
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
