2d45004b6b38f869116c3e4c4d5d8e1e095dbae4
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-02 08:22:22 +0200

    Add solution for day 2

---
+++ src/Day02.kt
@@ -0,0 +1,25 @@
+fun main() {
+    fun parse(s: String) = s.split(" ").map { it.toInt() }
+
+    fun List<Int>.isSafe(): Boolean {
+        val pairs = zipWithNext()
+        return pairs.all { (a, b) -> a - b in 1..3 } || pairs.all { (a, b) -> b - a in 1..3 }
+    }
+
+    fun List<Int>.subsets() =
+        indices.map { i -> withIndex().filter { it.index != i }.map { it.value } }
+
+    fun part1(input: List<String>) =
+        input.map { parse(it) }.count { it.isSafe() }
+
+    fun part2(input: List<String>) =
+        input.map { parse(it) }.count { it.subsets().any { it.isSafe() } }
+
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
