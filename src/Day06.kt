69b1667566d183e44a02dc59e71156d67da9e07c
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-06 14:47:38 +0200

    Optimization

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,24 +1,48 @@
-private data class LabMap(val rows: List<String>, val obstruction: Point? = null) {
+import kotlin.time.measureTime
+
+private data class LabMap(val rows: List<String>) {
 
     val coordinates = rows.first().indices.flatMap { x -> rows.indices.map { y -> Point(x, y) } }
     val start = coordinates.find { this[it] == '^' } ?: error("no start")
 
-    operator fun get(p: Point) =
-        if (p == obstruction) '#' else rows.getOrNull(p.y)?.getOrNull(p.x) ?: '_'
+    operator fun get(p: Point): Char? =
+        rows.getOrNull(p.y)?.getOrNull(p.x)
 
-    fun loops(): Boolean {
+    fun path(): Collection<Point> {
+        val seen = mutableSetOf<Point>()
+
         var d = CardinalDirection.N
         var p = start
-        val seen = mutableSetOf<Pair<Point, CardinalDirection>>()
 
-        while (this[p] != '_') {
-            if (!seen.add(p to d))
-                return true
+        while (this[p] != null) {
+            seen += p
 
             while (this[p + d] == '#')
                 d = d.clockwise()
 
             p += d
+        }
+
+        return seen
+    }
+
+    fun hasLoop(obstruction: Point): Boolean {
+        var d = CardinalDirection.N
+        var p = start
+        val seen = mutableSetOf<Pair<Point, CardinalDirection>>()
+
+        while (this[p] != null) {
+            var next = p + d
+
+            while (this[next] == '#' || next == obstruction) {
+                if (!seen.add(p to d))
+                    return true
+
+                d = d.clockwise()
+                next = p + d
+            }
+
+            p = next
         }
 
         return false
@@ -27,29 +51,12 @@ private data class LabMap(val rows: List<String>, val obstruction: Point? = null
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val map = LabMap(input)
-        val seen = mutableSetOf<Point>()
-
-        var d = CardinalDirection.N
-        var p = map.start
-
-        while (map[p] != '_') {
-            seen += p
-
-            while (map[p + d] == '#')
-                d = d.clockwise()
-
-            p += d
-        }
-
-        return seen.size
-    }
+    fun part1(input: List<String>): Int =
+        LabMap(input).path().size
 
     fun part2(input: List<String>): Int {
         val map = LabMap(input)
-
-        return map.coordinates.count { it != map.start && map.copy(obstruction = it).loops() }
+        return map.path().count { it != map.start && map.hasLoop(it) }
     }
 
     val testInput = readInputLines("Day06_test")
