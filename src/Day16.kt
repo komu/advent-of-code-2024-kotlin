6a6d6fbb1c0078e3b74d90eacbafaf60034b2b16
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-16 09:47:22 +0200

    Cleanup

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,76 +1,40 @@
-import CardinalDirection.E
-
 private class Maze(input: String) {
     val grid = CharGrid(input)
-    val start = grid.find('S') ?: error("no start")
-    val end = grid.find('E') ?: error("no end")
-    val startState = MazeState(start, E)
+    private val start = grid.find('S')!!
+    private val end = grid.find('E')!!
 
-    fun transitions(state: MazeState): List<Pair<MazeState, Int>> {
-        return listOf(
-            MazeState(state.point + state.direction, state.direction) to 1,
-            MazeState(state.point, state.direction.clockwise()) to 1000,
-            MazeState(state.point, state.direction.counterClockwise()) to 1000,
-        ).filter { (s, _) -> grid[s.point] != '#' }
+    val startState = MazeState(start, CardinalDirection.E)
+
+    fun isEnd(state: MazeState) = state.p == end
+
+    fun transitions(state: MazeState) = buildList {
+        val forward = state.p + state.d
+        if (grid[forward] != '#')
+            add(MazeState(forward, state.d) to 1)
+
+        add(MazeState(state.p, state.d.clockwise()) to 1000)
+        add(MazeState(state.p, state.d.counterClockwise()) to 1000)
     }
 
-    fun isEnd(state: MazeState) = state.point == end
+    data class MazeState(val p: Point, val d: CardinalDirection)
 }
 
-private data class MazeState(val point: Point, val direction: CardinalDirection)
-
-private class ChainLink(val state: MazeState, val previous: ChainLink?)
-
 fun main() {
-
     fun part1(input: String): Int {
         val maze = Maze(input)
-        return shortestPathCost(MazeState(maze.start, E), { it.point == maze.end }, maze::transitions)
+        return shortestPath(maze.startState, maze::isEnd, maze::transitions)?.cost ?: error("No path found")
     }
 
     fun part2(input: String): Int {
         val maze = Maze(input)
-
-        val max = shortestPathCost(maze.startState, maze::isEnd, maze::transitions)
-
-        val seenPoints = mutableSetOf<Point>()
-
-        val stack = mutableListOf(ChainLink(maze.startState, null))
-        var costs = mutableMapOf(maze.startState to 0)
-
-        while (stack.isNotEmpty()) {
-            val link = stack.removeFirst()
-            val state = link.state
-            val costToState = costs[state]!!
-            if (costToState > max) continue
-
-            if (maze.isEnd(state)) {
-                var l: ChainLink? = link
-                while (l != null) {
-                    seenPoints.add(l.state.point)
-                    l = l.previous
-                }
-            }
-
-            for ((s, c) in maze.transitions(state)) {
-                val nextCost = costToState + c
-                if (nextCost <= costs.getOrDefault(s, Int.MAX_VALUE)) {
-                    costs[s] = nextCost
-                    stack.add(ChainLink(s, link))
-                }
-            }
-        }
-
-        return seenPoints.size
+        val result = allShortestPaths(maze.startState, maze::isEnd, maze::transitions)
+        return result.paths.flatten().map { it.p }.toSet().size
     }
 
     check(part1(readInput("Day16_test")) == 7036)
     check(part1(readInput("Day16_test2")) == 11048)
-    check(part2(readInput("Day16_test")) == 45)
-    check(part2(readInput("Day16_test2")) == 64)
 
     val input = readInput("Day16")
     part1(input).println()
     part2(input).println()
 }
-
--- src/Dijkstra.kt
+++ src/Dijkstra.kt
@@ -1,44 +1,73 @@
 import java.util.*
 
-fun <T> shortestPathCost(from: T, isTarget: (T) -> Boolean, edges: (T) -> List<Pair<T, Int>>): Int =
-    shortestPathWithCost(from, isTarget, edges)?.second ?: error("no path")
+data class PathResult<T>(val cost: Int, val result: List<T>)
+data class PathsResult<T>(val cost: Int, val paths: List<List<T>>)
 
-fun <T> shortestPathWithCost(from: T, isTarget: (T) -> Boolean, edges: (T) -> List<Pair<T, Int>>): Pair<List<T>, Int>? {
+fun <T> shortestPath(from: T, isTarget: (T) -> Boolean, edges: (T) -> List<Pair<T, Int>>): PathResult<T>? {
     val initial = PathNode(from, null, 0)
-    val nodes = mutableMapOf(from to initial)
     val queue = PriorityQueue(setOf(initial))
-    val targets = mutableSetOf<T>()
+    var costs = mutableMapOf(from to 0)
 
     while (queue.isNotEmpty()) {
         val u = queue.remove()
 
-        for ((v, cost) in edges(u.point)) {
-            if (isTarget(v))
-                targets += v
+        if (isTarget(u.point)) {
+            return PathResult(u.totalCost, u.toList())
+        } else {
+            for ((v, cost) in edges(u.point)) {
+                val newCost = u.totalCost + cost
 
-            val newDistance = u.distance + cost
-            val previousNode = nodes[v]
-            if (previousNode == null || newDistance < previousNode.distance) {
-                val newNode = PathNode(v, u, newDistance)
-                nodes[v] = newNode
-                queue += newNode
+                if (newCost < costs.getOrDefault(v, Int.MAX_VALUE)) {
+                    costs[v] = newCost
+                    queue += PathNode(v, u, newCost)
+                }
             }
         }
     }
 
-    val targetNode = targets.map { nodes[it]!! }.minByOrNull { it.distance }
-    if (targetNode != null) {
-        val result = mutableListOf<T>()
-        var node: PathNode<T>? = targetNode
-        while (node?.previous != null) {
-            result += node.point
-            node = node.previous
-        }
-        return result.asReversed() to targetNode.distance
-    }
     return null
 }
 
-private class PathNode<T>(val point: T, val previous: PathNode<T>?, val distance: Int) : Comparable<PathNode<T>> {
-    override fun compareTo(other: PathNode<T>) = distance.compareTo(other.distance)
+fun <T> allShortestPaths(from: T, isTarget: (T) -> Boolean, edges: (T) -> List<Pair<T, Int>>): PathsResult<T> {
+    val initial = PathNode(from, null, 0)
+    val queue = PriorityQueue(setOf(initial))
+    val paths = mutableListOf<List<T>>()
+    var costs = mutableMapOf(from to 0)
+    var best = Int.MAX_VALUE
+
+    while (queue.isNotEmpty()) {
+        val u = queue.remove()
+
+        if (isTarget(u.point)) {
+            if (u.totalCost <= best) {
+                best = u.totalCost
+                paths += u.toList()
+            }
+
+        } else {
+            for ((v, cost) in edges(u.point)) {
+                val newCost = u.totalCost + cost
+
+                if (newCost <= costs.getOrDefault(v, Int.MAX_VALUE)) {
+                    costs[v] = newCost
+                    queue += PathNode(v, u, newCost)
+                }
+            }
+        }
+    }
+
+    return PathsResult(best, paths)
+}
+
+private class PathNode<T>(val point: T, val previous: PathNode<T>?, val totalCost: Int) : Comparable<PathNode<T>> {
+    override fun compareTo(other: PathNode<T>) = totalCost.compareTo(other.totalCost)
+
+    fun toList() = buildList {
+        var node: PathNode<T>? = this@PathNode
+        while (node != null) {
+            add(node.point)
+            node = node.previous
+        }
+        reverse()
+    }
 }
