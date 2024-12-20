84b62487985ba37aaf33daec2163adc54a3286d1
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-20 14:23:43 +0200

    Optimization

--- src/Day20.kt
+++ src/Day20.kt
@@ -3,9 +3,7 @@ import kotlin.time.measureTime
 
 fun main() {
 
-    data class Cheat(val startX: Int, val startY: Int, val endX: Int, val endY: Int)
-
-    fun solveCounts(input: String, distance: Int): Map<Int, Int> {
+    fun solveCounts(input: String, distance: Int, threshold: Int): Map<Int, Int> {
         val track = CharGrid(input)
         val start = track.find('S')!!
         val end = track.find('E')!!
@@ -14,15 +12,14 @@ fun main() {
         val normalCost = fromStart[end]
 
         val gainCounts = mutableMapOf<Int, Int>()
-        val cheats = mutableSetOf<Cheat>()
 
         for (startX in track.yRange) {
             for (startY in track.xRange) {
                 if (track[startX, startY] == '#') continue
 
                 for (dy in -distance..distance) {
-                    val xRange = distance - abs(dy)
-                    for (dx in -xRange..xRange) {
+                    val dxRange = distance - abs(dy)
+                    for (dx in -dxRange..dxRange) {
                         val endX = startX + dx
                         val endY = startY + dy
 
@@ -32,7 +29,7 @@ fun main() {
                         if (cheatCost > 1 && track.contains(endX, endY)) {
                             val cost = cheatCost + fromStart[startX, startY] + fromEnd[endX, endY]
                             val gain = normalCost - cost
-                            if (gain > 0 && cheats.add(Cheat(startX, startY, endX, endY)))
+                            if (gain >= threshold)
                                 gainCounts[gain] = gainCounts.getOrDefault(gain, 0) + 1
                         }
                     }
@@ -43,13 +40,13 @@ fun main() {
     }
 
     fun countOverThreshold(input: String, distance: Int, threshold: Int) =
-        solveCounts(input, distance).filterKeys { it >= threshold }.values.sum()
+        solveCounts(input, distance, threshold).values.sum()
 
     fun part1(input: String) = countOverThreshold(input, distance = 2, threshold = 100)
     fun part2(input: String) = countOverThreshold(input, distance = 20, threshold = 100)
 
     // @formatter:off
-    check(solveCounts(readInput("Day20_test"), 2) == mapOf(2 to 14, 4 to 14, 6 to 2, 8 to 4, 10 to 2, 12 to 3, 20 to 1, 36 to 1, 38 to 1, 40 to 1, 64 to 1))
+    check(solveCounts(readInput("Day20_test"), 2, 1) == mapOf(2 to 14, 4 to 14, 6 to 2, 8 to 4, 10 to 2, 12 to 3, 20 to 1, 36 to 1, 38 to 1, 40 to 1, 64 to 1))
     // @formatter:on
 
     val input = readInput("Day20")
