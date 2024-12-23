a63482316b08057783321e8f9537bb57a0a61cfb
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-23 08:44:32 +0200

    Optimization

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,6 +1,8 @@
+import kotlin.time.measureTime
+
 private class Graph(input: String) {
 
-    private val neighborsByNode = buildMap<String, MutableSet<String>> {
+    private val graph = buildMap<String, MutableSet<String>> {
         for (line in input.lines()) {
             val (x, y) = line.split('-')
             getOrPut(x) { mutableSetOf() }.add(y)
@@ -8,44 +10,80 @@ private class Graph(input: String) {
         }
     }
 
-    fun neighbors(node: String) = neighborsByNode[node].orEmpty()
+    val keys = graph.keys
 
-    fun findCliques(): Collection<Set<String>> {
-        val result = mutableListOf<Set<String>>()
+    fun neighbors(node: String) = graph[node].orEmpty()
+}
 
-        tailrec fun recurse(smallerCliques: Set<Set<String>>) {
-            if (smallerCliques.isEmpty()) return
-
-            result.addAll(smallerCliques)
-
-            val newCliques = mutableSetOf<Set<String>>()
-            for (clique in smallerCliques) {
-                val node = clique.first()
-                for (candidate in neighbors(node)) {
-                    if (clique.all { candidate in neighbors(it) }) {
-                        newCliques.add(clique + candidate)
-                    }
-                }
-            }
-
-            recurse(newCliques)
-        }
-
-        recurse(neighborsByNode.keys.map { setOf(it) }.toSet())
-
-        return result
+// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+private fun Graph.bronKerbosch(
+    R: Set<String>,
+    P: Set<String>,
+    X: Set<String>,
+    cliques: MutableList<Set<String>>
+) {
+    if (P.isEmpty() && X.isEmpty()) {
+        cliques.add(R) // R is a maximal clique
+        return
     }
+
+    val P = P.toMutableSet()
+    val X = X.toMutableSet()
+
+    // Optimization: choose a pivot to reduce recursive calls
+    val pivot = (P + X).firstOrNull() ?: return
+    val pivotNeighbors = neighbors(pivot)
+
+    for (v in P - pivotNeighbors) {
+        val neighbors = neighbors(v)
+        bronKerbosch(
+            R + v,
+            P.intersect(neighbors),
+            X.intersect(neighbors),
+            cliques
+        )
+        P.remove(v)
+        X.add(v)
+    }
+}
+
+private fun findMaximalCliques(g: Graph): List<Set<String>> {
+    val cliques = mutableListOf<Set<String>>()
+    g.bronKerbosch(
+        R = emptySet(),
+        P = g.keys,
+        X = emptySet(),
+        cliques = cliques
+    )
+    return cliques
 }
 
 fun main() {
 
-    fun part1(input: String) = Graph(input).findCliques().count { it.size == 3 && it.any { it.startsWith("t") } }
-    fun part2(input: String) = Graph(input).findCliques().maxBy { it.size }.sorted().joinToString(",")
+    fun part1(input: String): Int {
+        val g = Graph(input)
+        val result = mutableSetOf<String>()
+
+        for (a in g.keys.filter { it.startsWith('t') })
+            for (b in g.neighbors(a))
+                for (c in g.neighbors(a))
+                    if (b != c && b in g.neighbors(c))
+                        result.add(listOf(a, b, c).sorted().joinToString(","))
+
+        return result.size
+    }
+
+    fun part2(input: String) =
+        findMaximalCliques(Graph(input)).maxBy { it.size }.sorted().joinToString(",").trace("new")
 
     check(part1(readInput("Day23_test")) == 7)
     check(part2(readInput("Day23_test")) == "co,de,ka,ta")
 
     val input = readInput("Day23")
-    part1(input).println()
-    part2(input).println()
+    measureTime {
+        part1(input).println()
+    }.println()
+    measureTime {
+        part2(input).println()
+    }.println()
 }
