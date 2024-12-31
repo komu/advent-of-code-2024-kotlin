507af3007fc1d843c2bbcb6b11121b3365259312
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-31 09:44:13 +0200

    Fix code

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,28 +1,29 @@
 fun main() {
     fun parse(s: String) = s.split(" ").map { it.toInt() }
 
-    fun List<Int>.isSafe(range: IntRange, tolerate: Boolean): Boolean {
-        var mayIgnore = tolerate
+    fun List<Int>.isSafe(range: IntRange): Boolean {
 
         var prev = first()
         for (value in drop(1))
-            when {
-                value - prev in range -> prev = value
-                mayIgnore -> mayIgnore = false
-                else -> return false
-            }
+            if (value - prev in range)
+                prev = value
+            else
+                return false
 
         return true
     }
 
-    fun List<Int>.isSafe(tolerate: Boolean) =
-        isSafe(1..3, tolerate) || isSafe(-3..-1, tolerate)
+    fun List<Int>.isSafe() =
+        isSafe(1..3) || isSafe(-3..-1)
+
+    fun List<Int>.variationsWithSingleRemoved() =
+        indices.map { i -> withIndex().filter { it.index != i }.map { it.value } }
 
     fun part1(input: List<String>) =
-        input.count { parse(it).isSafe(tolerate = false) }
+        input.map { parse(it) }.count { it.isSafe() }
 
     fun part2(input: List<String>) =
-        input.count { parse(it).isSafe(tolerate = true) }
+        input.map { parse(it) }.count { it.variationsWithSingleRemoved().any { it.isSafe() } }
 
     val testInput = readInputLines("Day02_test")
     check(part1(testInput) == 2)
@@ -31,4 +32,7 @@ fun main() {
     val input = readInputLines("Day02")
     part1(input).println()
     part2(input).println()
+
+    assertEquals(part1(input), 332)
+    assertEquals(part2(input), 398)
 }
